f50ef68ff38e3b1dea784870528e66c095cb1615
Lumine_ <grace.estrada@ftwfoundation.org>
2024-08-06 15:24:30 +0800

    feature: added rate limiter | 5 requests/minute

--- server/app.py
+++ server/app.py
@@ -1,18 +1,33 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Query, Request, HTTPException
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+from fastapi.responses import JSONResponse
+from typing import List, Dict, Any
+from dataclasses import dataclass
 from game import simulate_baccarat
 from game import dataclass_to_dict
-from dataclasses import asdict
-from typing import Dict, Any
 
+
+# Instantiate limiter
+limiter = Limiter(key_func=get_remote_address)
+
+# Instantiate FastAPI with rate limiter
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
+
+# FastAPI routing with number of records exception handling
 @app.get("/baccarat")
-def get_baccarat(num_records = 1):
-    if int(num_records) <= 100:
-        records = simulate_baccarat(int(num_records))
+@limiter.limit("5/minute") 
+async def get_baccarat(request: Request, num_records: int = Query(1, le=100)):
+    if num_records <= 100:
+        records = simulate_baccarat(num_records)
         return [dataclass_to_dict(record) for record in records]
+        
     else:
         raise HTTPException(
-            status_code = 400,
-            detail = 'The number of records requested exceeds the limit of 100.'
+            status_code=400,
+            detail="The number of records requested exceeds the limit of 100."
         )
